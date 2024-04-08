9df581f2e2612b0bd5dee8eed4d6df236a4023b7
Dea Noveriyanti <95268820+Deoyski@users.noreply.github.com>
2024-04-08 08:16:15 +0700

    Update rule base

--- Frontend/src/Components/Question.jsx
+++ Frontend/src/Components/Question.jsx
@@ -8,13 +8,12 @@ function Question() {
   const navigate = useNavigate();
   const location = useLocation();
   const formData = location.state ? location.state.formData : null;
-  const [additionalQuestionsFetched, setAdditionalQuestionsFetched] = useState(false);
-
-  const [questionIndex, setQuestionIndex] = useState(0); // Indeks pertanyaan saat ini
-  const [answers, setAnswers] = useState([]); // Array untuk menyimpan jawaban pengguna
-  const [questions, setQuestions] = useState([]); // Arr
-  const [answerCodes, setAnswerCodes] = useState([]); // Array untuk menyimpan kode pertanyaan
-
+  const [additionalQuestionsFetched, setAdditionalQuestionsFetched] =
+    useState(false);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState([]); // Menyimpan jawaban pengguna
+  const [questions, setQuestions] = useState([]); // Menyimpan pertanyaan
+  const [answerCodes, setAnswerCodes] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -29,12 +28,18 @@ function Question() {
     fetch("http://localhost:5000/ciri-jenazah")
       .then((response) => response.json())
       .then((ciriJenazahData) => {
-        // Menyaring pertanyaan P5, P6, dan P7
-        const filteredQuestions = ciriJenazahData.filter(question => question.kode === "P5" || question.kode === "P6" || question.kode === "P7");
+        const filteredQuestions = ciriJenazahData.filter(
+          (question) =>
+            question.kode === "P5" ||
+            question.kode === "P6" ||
+            question.kode === "P23" ||
+            question.kode === "P34" ||
+            question.kode === "P7"
+        );
         // Mengurutkan pertanyaan berdasarkan urutan yang diinginkan
         const sortedQuestions = filteredQuestions.sort((a, b) => {
-          // Menetapkan urutan untuk P5, P6, dan P7
-          const order = { "P5": 0, "P6": 1, "P7": 2 };
+          // Menetapkan urutan keluar pertanyaan
+          const order = { P5: 0, P6: 1, P7: 2, P23: 3, P34: 4 };
           return order[a.kode] - order[b.kode];
         });
         // Set pertanyaan yang sudah difilter dan diurutkan ke dalam state
@@ -45,49 +50,125 @@ function Question() {
 
   const handleAnswer = (answer) => {
     const currentQuestion = questions[questionIndex];
-    const answerText = `${currentQuestion.keterangan} - ${answer ? 'iya' : 'tidak'}`;
-     const answerCode = currentQuestion.id; // Mendapatkan kode pertanyaan
+    const answerText = `${currentQuestion.keterangan} - ${
+      answer ? "IYA" : "TIDAK"
+    }`;
+    const answerCode = currentQuestion.id; // Mendapatkan kode pertanyaan
 
     if (answer) {
-      setAnswerCodes([...answerCodes, answerCode]);
-      setAnswers([...answers, answerText]);
+    
+        setAnswerCodes([...answerCodes, answerCode]);
+        setAnswers([...answers, answerText]);
+
     } else {
       setAnswers([...answers, answerText]);
     }
-    console.log(answers);
-    console.log(answerCodes);
+    console.log(answerCode);
 
-    
     if (questionIndex + 1 < questions.length) {
-      setQuestionIndex(questionIndex + 1);     
-    } else {
-      if (!additionalQuestionsFetched) {
-        fetchAdditionalQuestions();
-      } else {
-        navigateToTestResult();
+      setQuestionIndex(questionIndex + 1);
+    } else if (!additionalQuestionsFetched) {
+      // Mengambil jawaban empat pertanyaan sebelumnya
+      const previousAnswers = answers.slice(Math.max(0, questionIndex - 4));
+      console.log("Jawaban sebelumnya:", previousAnswers);
+
+      const P5_Yes = previousAnswers
+        .filter((_, index) => questions[index].kode === "P5")
+        .every((answer) => answer.endsWith("IYA"));
+      const P6_Yes = previousAnswers
+        .filter((_, index) => questions[index].kode === "P6")
+        .every((answer) => answer.endsWith("IYA"));
+      const P7_Yes = previousAnswers
+        .filter((_, index) => questions[index].kode === "P7")
+        .every((answer) => answer.endsWith("IYA"));
+      const P23_Yes = previousAnswers
+        .filter((_, index) => questions[index].kode === "P23")
+        .every((answer) => answer.endsWith("IYA"));
+
+      const P5_No = previousAnswers
+        .filter((_, index) => questions[index].kode === "P5")
+        .every((answer) => answer.endsWith("TIDAK"));
+      const P6_No = previousAnswers
+        .filter((_, index) => questions[index].kode === "P6")
+        .every((answer) => answer.endsWith("TIDAK"));
+      const P7_No = previousAnswers
+        .filter((_, index) => questions[index].kode === "P7")
+        .every((answer) => answer.endsWith("TIDAK"));
+      const P23_No = previousAnswers
+        .filter((_, index) => questions[index].kode === "P23")
+        .every((answer) => answer.endsWith("TIDAK"));
+
+      if (P6_Yes && P7_Yes && P5_No && P23_No) {
+        fetchAdditionalQuestions([
+          "P1",
+          "P2",
+          "P3",
+          "P9",
+          "P10",
+          "P17",
+          "P18",
+          "P35",
+        ]);
+      } else if (P5_Yes && P6_No && P7_No && P23_No) {
+        fetchAdditionalQuestions([
+          "P8",
+          "P12",
+          "P16",
+          "P20",
+          "P22",
+          "P26",
+          "P32",
+          "P33",
+          "P35",
+        ]);
+      } else if (P5_Yes && P6_No && P7_Yes && P23_No) {
+        fetchAdditionalQuestions(["P11", "P19", "P21", "P22", "P35"]);
+      } else if (P5_Yes && P23_Yes && P6_No && P7_No) {
+        fetchAdditionalQuestions([
+          "P13",
+          "P14",
+          "P15",
+          "P24",
+          "P25",
+          "P26",
+          "P27",
+          "P28",
+          "P29",
+          "P30",
+          "P31",
+          "P35",
+        ]);
+      } else if (P5_No && P6_No && P7_Yes && P23_No) {
+        fetchAdditionalQuestions(["P3", "P4", "P10", "P11", "P18", "P35"]);
       }
+    } else {
+      navigateToTestResult();
     }
   };
 
-  const fetchAdditionalQuestions = () => {
+  const fetchAdditionalQuestions = (additionalQuestionCodes) => {
     fetch("http://localhost:5000/ciri-jenazah")
       .then((response) => response.json())
       .then((ciriJenazahData) => {
-        const additionalQuestions = ciriJenazahData.filter(question => ["P1", "P2", "P9", "P11"].includes(question.kode));
-        setQuestions(additionalQuestions);
-        setQuestionIndex(0);
-        setAdditionalQuestionsFetched(true);
+        const additionalQuestions = ciriJenazahData.filter((question) =>
+          additionalQuestionCodes.includes(question.kode)
+        );
+        if (additionalQuestions.length > 0) {
+          setQuestions(additionalQuestions);
+          setQuestionIndex(0);
+          setAdditionalQuestionsFetched(true); // Tandai bahwa pertanyaan tambahan sudah diambil
+        }
       })
       .catch((error) => console.error(error));
   };
-
+  
   const navigateToTestResult = () => {
     navigate("/test_result", {
       state: {
         formData: formData,
         answers: answers,
-        answerCodes: answerCodes
-      }
+        answerCodes: answerCodes,
+      },
     });
   };
 
@@ -121,14 +202,20 @@ function Question() {
               Question
             </div>
             <div className="py-20 text-2xl text-center text-[#002259]">
-            {renderQuestion()} 
+              {renderQuestion()}
             </div>
             <div className="flex text-white font-bold justify-center w-full">
               <div className="w-10/12 lg:flex gap-4">
-              <button onClick={() => handleAnswer(true)} className="lg:w-6/12 rounded-md w-full bg-[#F3B320] px-10 py-3">
+                <button
+                  onClick={() => handleAnswer(true)}
+                  className="lg:w-6/12 rounded-md w-full bg-[#F3B320] px-10 py-3"
+                >
                   Ya
                 </button>
-                <button onClick={() => handleAnswer(false)} className="lg:w-6/12 rounded-md w-full bg-[#002259] px-10 py-3 lg:mt-0 mt-2">
+                <button
+                  onClick={() => handleAnswer(false)}
+                  className="lg:w-6/12 rounded-md w-full bg-[#002259] px-10 py-3 lg:mt-0 mt-2"
+                >
                   Tidak
                 </button>
               </div>
--- Frontend/src/Components/TestResult.jsx
+++ Frontend/src/Components/TestResult.jsx
@@ -1,17 +1,101 @@
 import MainNavbar from "./MainNavbar";
 import background from "../assets/background.png";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function TestResult() {
   const location = useLocation();
   const formData = location.state ? location.state.formData : null;
   const answers = location.state ? location.state.answers : [];
-  const answerCodes = location.state ? location.state.answerCodes : [];
+  // const answerCodes = location.state ? location.state.answerCodes : [];
+  const answerCodes = location.state ? location.state.answerCodes.slice() : [];
+const index = answerCodes.indexOf(46);
+if (index !== -1) {
+  answerCodes.splice(index, 1);
+}
+  const [data, setDataRuleBase] = useState([]);
+  const [timeOfDeath, setTimeOfDeath] = useState(null);
 
-  // Log formData and answers to console
-  console.log("formData:", formData);
-  console.log("answers:", answers);
-  console.log("answers codes:", answerCodes);
+  // Fetch Data
+  useEffect(() => {
+    fetch("http://localhost:5000/rule-base")
+      .then((response) => response.json())
+      .then((data) => {
+        setDataRuleBase(data);
+        const matchedIntervalIds = determineIntervalId(data, answerCodes);
+        if (matchedIntervalIds.length > 0) {
+          // Jika ada interval ID yang cocok, ambil data interval untuk interval ID pertama yang cocok
+          fetchIntervalData(matchedIntervalIds[0]);
+        } else {
+          console.log("Tidak ada interval yang cocok dengan data rule base.");
+        }
+      })
+      .catch((error) => console.error(error));
+  }, [answerCodes]);
+
+  const determineIntervalId = (ruleBaseData, answerCodes) => {
+    // Buat objek untuk menyimpan jumlah kemunculan cirijenazahId untuk setiap intervalId
+    const intervalCount = {};
+
+    // Inisialisasi set answerCodes untuk memudahkan pencarian
+    const matchedCiriJenazahIds = new Set(answerCodes);
+
+    // Iterasi setiap item pada data rule base
+    ruleBaseData.forEach((item) => {
+      const intervalId = item.intervalId;
+      const ciriJenazahId = item.ciriJenazahId;
+
+      // Periksa apakah ciriJenazahId ada di dalam answerCodes
+      if (matchedCiriJenazahIds.has(ciriJenazahId)) {
+        // Tambahkan intervalId ke intervalCount jika belum ada
+        if (!intervalCount[intervalId]) {
+          intervalCount[intervalId] = new Set();
+        }
+        intervalCount[intervalId].add(ciriJenazahId);
+      }
+    });
+
+    // Temukan intervalId yang memiliki semua cirijenazah yang cocok
+    let matchedIntervalId = null;
+
+    for (const [intervalId, ciriJenazahSet] of Object.entries(intervalCount)) {
+      if (ciriJenazahSet.size === matchedCiriJenazahIds.size) {
+        // Jika set cirijenazahId pada intervalId sama dengan set answerCodes,
+        // maka intervalId ini cocok dengan semua cirijenazah
+        matchedIntervalId = intervalId;
+        break; // Keluar dari loop karena sudah ditemukan intervalId yang cocok
+      }
+    }
+
+    return matchedIntervalId;
+  };
+
+  const fetchIntervalData = (matchedIntervalId) => {
+    // console.log(matchedIntervalId);
+    fetch(`http://localhost:5000/interval/${matchedIntervalId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log("ini data interval", data);
+        const mostFrequentTimeOfDeath = data.keterangan;
+        // Check if timeOfDeath is already set
+        if (timeOfDeath === null) {
+          setTimeOfDeath(mostFrequentTimeOfDeath);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        // If there's an error, set timeOfDeath to default value
+        setTimeOfDeath("Ciri Jenazah belum dapat ditentukan");
+      });
+  };
+  
+
+  // console.log("rulebase :", data);
+  // // Log formData and answers to console
+  // console.log("formData:", formData);
+  // console.log("answers:", answers);
+  // console.log("answers codes:", answerCodes);
   return (
     <>
       <MainNavbar />
@@ -19,17 +103,23 @@ function TestResult() {
         <div className="absolute inset-0 w-full h-full bg-cover bg-no-repeat">
           <img className="w-full h-full object-cover" src={background} alt="" />
         </div>
-        <div className="container relative flex-col lg:w-[68%] w-[85%] h-[78%] flex justify-center">
+        <div className="container relative flex-col lg:w-[68%] w-[85%] h-[77%] flex justify-center">
           <div className=" bg-white flex-col h-full rounded-3xl items-center overflow-y-auto lg:px-16 px-10 shadow-xl">
-            <div className="flex justify-between pt-16">
-              <div className="text-5xl font-extrabold lg:flex   text-[#002259]">
+            <div className="flex justify-between pt-10">
+              <div className="text-4xl font-extrabold lg:flex   text-[#002259]">
                 Test Result
               </div>
-              <button className="bg-[#F3B320] text-white px-10 py-2 rounded-md">
-                Cetak Pdf
+              <div className="flex gap-3">
+                  <button className="bg-[#F3B320] text-white px-10 py-2 rounded-md">
+                Print Hasil
               </button>
+              <button className="bg-[#F3B320] text-white px-10 py-2 rounded-md">
+                Simpan Hasil
+              </button>
+              </div>
+            
             </div>
-            <div className="lg:flex pt-14">
+            <div className="lg:flex pt-12">
               <div className="lg:w-4/12 flex flex-col justify-start gap-4">
                 <div className="flex flex-col gap-1">
                   <p className="font-bold">Nomor Pemeriksaan</p>
@@ -49,7 +139,9 @@ function TestResult() {
                 </div>
                 <div className="flex flex-col gap-1">
                   <p className="font-bold">Perkiraan Umur</p>
-                  <p className="text-gray-500">{formData.perkiraan_umur} tahun</p>
+                  <p className="text-gray-500">
+                    {formData.perkiraan_umur} tahun
+                  </p>
                 </div>
                 <div className="flex flex-col gap-1">
                   <p className="font-bold">Tanggal dan Jam Penemuan</p>
@@ -59,34 +151,43 @@ function TestResult() {
                   <p className="font-bold">Lokasi Penemuan</p>
                   <p className="text-gray-500">{formData.lokasi_penemuan}</p>
                 </div>
-              </div>
-              <div className="lg:w-8/12 flex flex-col justify-start gap-4 lg:border-s-2 lg:border-black lg:pl-10">
                 <div className="flex flex-col gap-1">
                   <p className="font-bold">Informasi Tambahan</p>
                   <p className="text-gray-500">{formData.informasi_tambahan}</p>
                 </div>
+              </div>
+
+              <div className="lg:w-8/12 flex flex-col justify-start gap-4 lg:border-s-2 lg:border-black lg:pl-10">
                 <div className="flex flex-col gap-1">
                   <p className="font-bold">
-                  Ciri yang ditemukan pada Jenazah :
+                    Ciri yang ditemukan pada Jenazah :
                   </p>
                   <ol className="text-gray-500 list-decimal pl-4">
-                  {answers.map((answer, index) => (
-                    <li key={index} className="text-gray-500">
-                      {answer}
-                    </li>
-                  ))}
+                    {answers
+                      // Filter pertanyaan dengan kode "P34"
+                  
+                      .map((answer, index) => (
+                        <li key={index} className="text-gray-500">
+                          {answer}
+                        </li>
+                      ))}
                   </ol>
                 </div>
                 <div className="flex flex-col gap-1">
                   <p className="font-bold">Waktu Sejak Kematian</p>
-                  <p className="text-gray-500">2-3 Jam</p>
+                  <p className="text-gray-500">
+                    {timeOfDeath ? timeOfDeath : "Belum ada interval waktu saat kematian yang tepat"}
+                  </p>
                 </div>
               </div>
             </div>
-            <div className="flex items-center justify-center py-12 w-full">
-              <button className="bg-[#F3B320] w-9/12 text-white py-3 px-4 rounded">
+            <div className="flex items-center justify-center py-6 w-full">
+              <Link
+                className="bg-[#F3B320] w-9/12 text-white text-center py-3 px-4 rounded"
+                to="/"
+              >
                 Kembali ke Home
-              </button>
+              </Link>
             </div>
           </div>
         </div>
